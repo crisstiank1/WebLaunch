9fa3525dea1f40c8a23b78a3d89f1540e341282b
crisstiank1 <cristiancuentaff11@gmail.com>
2025-04-06 11:51:42 -0500

    Proyecto funcionando con pruebas en Jest

--- script.test.js
+++ script.test.js
@@ -1,10 +1,5 @@
-// suma
-function sum(a, b) {
-    return a + b;
-  }
-  
-  test('suma 2 + 3 debe ser igual a 5', () => {
-    expect(sum(2, 3)).toBe(5);
-  });
-  
+const { sum } = require('./script');
 
+test('suma 2 + 3 debe ser igual a 5', () => {
+  expect(sum(2, 3)).toBe(5);
+});
